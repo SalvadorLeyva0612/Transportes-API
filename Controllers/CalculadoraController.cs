745aab5c333c228f9f78c356fc53ed0f6f0c5174
JUAN SALVADOR LEYVA ZARATE <id20102@dasc.edu.mx>
2024-09-27 09:56:46 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/CalculadoraController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System.Transactions;
+
+namespace Transportes_API.Controllers
+{
+
+
+    //Decoradores/ Data Annotations
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculadoraController : ControllerBase
+    {
+        //GET
+        [HttpGet]
+        [Route("suma")]
+        public IActionResult suma(double a, double b)
+        {
+            double resultado = a + b;
+            return Ok(new { resultado });
+        }
+
+        [HttpGet]
+        [Route("resta")]
+        public IActionResult resta(double a, double b)
+        {
+            double resultado = a - b;
+            return Ok(new { resultado });
+        }
+
+       
+
+        public class operadores
+        { 
+        public double a { get; set; }
+
+        public double b { get; set; }
+        }
+
+        [HttpPost]
+        [Route ("multiplicacion")]
+        public IActionResult multiplicacion(operadores operador)
+        {
+            double resultado = operador.a * operador.b;
+            return Ok(new { resultado});
+        }
+
+        [HttpPost]
+        [Route("division")]
+        public IActionResult division(operadores operador)
+        {
+            double resultado = operador.a / operador.b;
+            return Ok(new { resultado });
+        }
+    }
+}
