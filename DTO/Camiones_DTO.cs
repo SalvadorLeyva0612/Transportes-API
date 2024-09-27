ce1b66eb449625f06210670c73298bb09400dde7
JUAN SALVADOR LEYVA ZARATE <id20102@dasc.edu.mx>
2024-09-27 11:02:12 -0600

    [ADD]- creación de proyecto Camion-DTO

---
+++ DTO/Camiones_DTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Camiones_DTO
+    {
+        
+        public int ID_camiones { get; set; }
+        
+        public string matricula { get; set; }
+
+        public int capacidad { get; set; }
+        
+        public string tipo_camion { get; set; }
+        public string urlfoto { get; set; }
+        
+        public string marca { get; set; }
+        public string modelo { get; set; }
+
+        public double kilometraje { get; set; }
+
+        public bool disponibilidad { get; set; }
+    }
+}
